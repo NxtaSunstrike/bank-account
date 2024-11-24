b8a28ae10ade81e9b13603a0e3bf9b862a64ddcb
NxtaSunstrike <unixalle765@mail.ru>
2024-11-24 06:46:56 -0500

    update account

--- app/domain/account.py
+++ app/domain/account.py
@@ -1,13 +1,14 @@
 from typing import Self
 from dataclasses import dataclass
 
+from app.domain.event_manager import EventManager
 from app.domain.value_objects import AccountId, Amount
 from app.domain.events import AccountCreated, AccountCredited, AccountDebited, WithdrawalLimitChanged
 from app.domain.exceptions import WithdrawError
 
 
 @dataclass
-class Account:
+class Account(EventManager):
     id: AccountId
     balance: Amount
     withdrawal_limit: Amount
@@ -16,15 +17,17 @@ class Account:
     def create_account(
         id: AccountId, balance: Amount = 0.0
     ) -> 'Account': 
-        event: AccountCreated = AccountCreated(
-            account_id=id.id,
-            balance=balance.amount
-        )
-        return Account(
+        account: Account = Account(
             id=id, 
             balance=balance,
             withdrawal_limit=Amount(1000.0)
         )
+        event: AccountCreated = AccountCreated(
+            account_id=id.id,
+            balance=balance.amount
+        )
+        account.add_event(event=event)
+        return Account
 
     def withdraw(self: Self, amount: Amount) -> None:
         if amount > self.withdrawal_limit.amount:
@@ -38,6 +41,7 @@ class Account:
             account_id=self.id.id,
             amount=amount.amount
         )
+        self.add_event(event=event)
     
     def deposit(self: Self, amount: Amount) -> None:
         self.balance.amount += amount.amount
@@ -46,6 +50,7 @@ class Account:
             amount=amount.amount
         )
         self._upgrade_limit(amount=Amount(500.0))
+        self.add_event(event=event)
 
     def _upgrade_limit(self: Self, amount: Amount) -> None:
         self.withdrawal_limit.amount += amount.amount
@@ -53,3 +58,4 @@ class Account:
             account_id=self.id.id,
             withdrawal_limit=self.withdrawal_limit.amount
         )
+        self.add_event(event=event)
