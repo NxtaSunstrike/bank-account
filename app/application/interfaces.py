02adee17bb65597dec5e1908a7f7cb025584438d
NxtaSunstrike <unixalle765@mail.ru>
2024-11-24 07:50:13 -0500

    edit interfaces

--- app/application/interfaces.py
+++ app/application/interfaces.py
@@ -1,8 +1,11 @@
 from typing import Self, Protocol
 from abc import abstractmethod
+from decimal import Decimal
 
 from app.domain.events import BaseDomainEvent
 from app.domain.value_objects import AccountId
+from app.domain.bank_account import BankAccount
+from app.domain.value_objects import Amount
 
 
 class IventBusInterface(Protocol):
@@ -29,3 +32,27 @@ class UnitOfWorkInterface(Protocol):
     @abstractmethod
     async def rollback(self: Self) -> None:
         raise NotImplementedError("rollback() must be implemented")
+
+
+class AccountReader(Protocol):
+    @abstractmethod
+    async def get_account_balance(self: Self, account_id: AccountId) -> Amount:
+        raise NotImplementedError("get_account_balance() must be implemented")
+    
+    @abstractmethod
+    async def get_account(self: Self, account_id: AccountId) -> BankAccount:
+        raise NotImplementedError("get_account() must be implemented")
+
+
+class AccountWriter(Protocol):
+    @abstractmethod
+    async def create_account(self: Self, account_id: AccountId, balance: Amount) -> None:
+        raise NotImplementedError("save_account() must be implemented")
+    
+    @abstractmethod
+    async def credit_account(self: Self, account_id: AccountId, amount: Amount) -> None:
+        raise NotImplementedError("credit_account() must be implemented")
+    
+    @abstractmethod
+    async def debit_account(self: Self, account_id: AccountId, amount: Amount) -> None:
+        raise NotImplementedError("debit_account() must be implemented")
