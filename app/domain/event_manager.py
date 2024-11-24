4fa93fc415ebe753c94f8741ab91998dba903abe
NxtaSunstrike <unixalle765@mail.ru>
2024-11-24 06:43:51 -0500

    add event manager

---
+++ app/domain/event_manager.py
@@ -0,0 +1,17 @@
+from typing import Self
+from dataclasses import dataclass, field
+
+from app.domain.events import BaseDomainEvent
+
+
+@dataclass
+class EventManager:
+    _events: list[BaseDomainEvent] = field(default_factory=list, init=False)
+
+    def add_event(self: Self, event: BaseDomainEvent) -> None:
+        self._events.append(event)
+
+    def push_events(self: Self) -> list[BaseDomainEvent]:
+        events: list[BaseDomainEvent] = self._events.copy()
+        self._events.clear()
+        return self._events
